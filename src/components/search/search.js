09b1902f64492664de1baa3946b1657719238487
Jayy1287 <sanjayrajakumar17@gmail.com>
2023-01-09 12:40:28 +0530

    first commit

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from "../../api";
+import { GEO_API_URL, geoApiOptions, NEW_WEATHER_API_URL, NEW_WEATHER_API_KEY } from "../../api";
 
 
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
+        if (inputValue.length < 3) {
+            return {
+                options: []
+            }
+        }
         return fetch(
-            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-            geoApiOptions
+            // `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            `http://api.weatherapi.com/v1/search.json?key=${NEW_WEATHER_API_KEY}&q=${inputValue}`
+            
+            // geoApiOptions
         )
             .then((response) => response.json())
             .then((response) => {
+                console.log(response);
                 return {
-                    options: response.data.map((city) => {
+                    // options: []
+                    options: response.map((city) => {
                         return {
-                            value: `${city.latitude} ${city.longitude}`,
-                            label: `${city.name}`,
+                            value: ``,
+                            label: `${city.name}` 
+                            
                         }
                     })
                 }
